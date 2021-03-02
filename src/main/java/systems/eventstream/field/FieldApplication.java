2c8402b0ba2da95056cd2d4b4cf830fee51f6375
Michael O'Brien <michael@obrienlabs.org>
2021-03-01 21:27:24 -0500

    #1 - adjust for 2 main method application classes

--- src/main/java/systems/eventstream/field/FieldApplication.java
+++ src/main/java/systems/eventstream/field/FieldApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FieldApplication {
 
+	// OpenAPI2SpringBoot has the single main method
 	public static void main(String[] args) {
 		SpringApplication.run(FieldApplication.class, args);
 	}
