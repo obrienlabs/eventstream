7c3ea34047b5ef87efaa7c7cee7a6094478efabc
Michael O'Brien <michael@obrienlabs.org>
2021-03-01 21:17:18 -0500

    #1 - initial springboot archetype

---
+++ src/main/java/systems/eventstream/field/FieldApplication.java
@@ -0,0 +1,13 @@
+package systems.eventstream.field;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldApplication.class, args);
+	}
+
+}
